febecc31dacd8b697f253303210311d765e725f6
guifroes1984 <guilhermefroes26@gmail.com>
2023-03-13 16:01:24 -0300

    categoria-read foi implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -37,6 +38,7 @@ import { CategoriaReadComponent } from './components/views/categoria/categoria-r
     MatIconModule,
     MatCardModule,
     MatTableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
